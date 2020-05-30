b21b34335bd83250fe99bb512898ed85019a9c01
Tanya311 <55144291+Tanya311@users.noreply.github.com>
2020-05-30 10:41:36 +0300

    Реализовывает слайдер, пока без анимации

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,30 +1,57 @@
-let btn_prev = document.querySelector('.button--prev');
-let btn_next = document.querySelector('.button--next');
+'use strict'
+const IMAGE_COUNT = 3;
+let btnPrev = document.querySelector('.button--prev');
+let btnNext = document.querySelector('.button--next');
 
-let images = document.querySelectorAll('.galery__photo');
-let i = 0;
+let imageFontBlock = document.querySelector('.galery__photo--showed-front');
+let imageBackBlock = document.querySelector('.galery__photo--showed-back');
+let imageFront = document.querySelector('.galery__photo--showed-front img');
+let imageBack = document.querySelector('.galery__photo--showed-back img');
+let i = 1;
+let j = 3;
 
 
+let changeImageNext = function() {
+  i++;
+  j++;
 
-btn_next.onclick = function(){
-     i = i + 1; /* i++ */
+  if (i > IMAGE_COUNT) {
+    i = 1;
+  }
 
-     if(i >= images.length){
-         i = 1;
-     }
+  if (j > IMAGE_COUNT) {
+    j = 1;
+  }
 
-     alert(i);
-     images[i - 1].classList.add('galery__photo--showed-back');
-     images[i - 1].classList.remove('galery__photo--none');
-     images[i - 1].classList.remove('galery__photo--showed-front');
+imageFront.src = 'img/photo-slide' + i + ".jpg";
+imageBack.src = 'img/photo-slide' + j + ".jpg";
+};
 
-     images[i].classList.add('galery__photo--showed-front');
-     images[i].classList.remove('galery__photo--showed-back');
-     images[i].classList.remove('galery__photo--none');
+let changeImagePrev = function () {
+  i--;
+  j--;
+  if (i < 1) {
+    i = 3;
+  }
 
-     images[i + 1].classList.add('galery__photo--none');
-     images[i + 1].classList.remove('galery__photo--showed-front');
-     images[i + 1].classList.remove('galery__photo--showed-back');
+  if (j < 1) {
+    j = 3;
+  }
 
-}
+  imageFront.src = 'img/photo-slide' + i + ".jpg";
+  imageBack.src = 'img/photo-slide' + j + ".jpg";
+};
+
+let btnNextClickHandler = function () {
+  changeImageNext();
+};
+
+let btnPrevClickHandler = function () {
+  changeImagePrev();
+};
+
+btnNext.addEventListener('click', btnNextClickHandler);
+btnPrev.addEventListener('click', btnPrevClickHandler);
+
+
 
